1ce842ca0d06805f011802908e75de7bf06452ad
David Becker <david@beckersite.com>
2017-10-17 23:24:20 -0600

    Add user interaction for web layout

--- accordion.js
+++ accordion.js
@@ -1,6 +1,18 @@
-//if Tab2 is active, turn off border-right for Tab1
-//if Tab3 is active, turn off border-right for Tab2
-//if Tab4 is active, turn off border-right for Tab3
+$(window).on('resize', () => {
+  const window = $(this);
+  if (window.width() < 600) {
+    $('.acc-article').each((i, elem) => {
+      $(elem).show()
+      if ($(elem).hasClass('acc-article-active')) {
+        $(elem).children('.tab').children('.fa').addClass('fa-minus')
+        $(elem).children('.tab').children('.fa').removeClass('fa-plus')
+      } else {
+        $(elem).children('.tab').children('.fa').removeClass('fa-minus')
+        $(elem).children('.tab').children('.fa').addClass('fa-plus')
+      }
+    })
+  }
+})
 
 $('.mobile-tab').on('click', (e) => {
   const selectedArticle = $(e.target).parent().find('.acc-content');
@@ -19,5 +31,27 @@ $('.mobile-tab').on('click', (e) => {
     selectedArticle.addClass('acc-content-active')
     selectedTabBtn.addClass('fa-minus')
     selectedTabBtn.removeClass('fa-plus')
+  }
+})
+
+$('.tab-container').on('click', '.tab', (e) => {
+  const tab = $(e.target).hasClass('tab') ? $(e.target) : $(e.target).parent();
+  const tabText = $(tab).children().text()
+  if (!$(tab).hasClass('tab-active')) {
+    $('.tab').each((i, elem) => {
+      $(elem).removeClass('tab-active')
+    })
+    $('.acc-article').each((i, elem) => {
+      $(elem).children('.acc-content').removeClass('acc-content-active')
+      $(elem).removeClass('acc-article-active')
+      $(elem).hide()
+      if ($(elem).children().children('p').text() === tabText) {
+        $(elem).addClass('acc-article-active')
+        $(elem).children('.acc-content').addClass('acc-content-active')
+        $(elem).addClass('acc-article-active')
+        $(elem).show()
+      }
+    })
+    $(tab).addClass('tab-active')
   }
 })
