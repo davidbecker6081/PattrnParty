0c2ebc7d7571efd625c5cf0e0ae425b781af35a6
David Becker <david@beckersite.com>
2017-10-17 22:20:45 -0600

    Clean up JS file

--- accordion.js
+++ accordion.js
@@ -1,17 +1,6 @@
 //if Tab2 is active, turn off border-right for Tab1
 //if Tab3 is active, turn off border-right for Tab2
 //if Tab4 is active, turn off border-right for Tab3
-// tab tab-active mobile-tab
-// acc-content
-
-const maximizeTabMobile = (container) => {
-
-}
-
-const minimizeOtherTabsMobile = (containerArray) => {
-
-}
-
 
 $('.mobile-tab').on('click', (e) => {
   const selectedArticle = $(e.target).parent().find('.acc-content');
