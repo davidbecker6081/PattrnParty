d1d42e3ba3d9d74492fe4c567c7f5324e95d9733
David Becker <david@beckersite.com>
2017-10-17 18:29:06 -0600

    Finish mobile stylings

--- accordion.js
+++ accordion.js
@@ -1,3 +1,5 @@
 //if Tab2 is active, turn off border-right for Tab1
 //if Tab3 is active, turn off border-right for Tab2
 //if Tab4 is active, turn off border-right for Tab3
+// tab tab-active mobile-tab
+// acc-content
